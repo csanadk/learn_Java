e61ec8c2b6a97ce557a1a10c443471b26268565b
csanadk <49592602+csanadk@users.noreply.github.com>
2025-07-01 07:14:50 +0200

    Create Switch_Statement.java

---
+++ Switch_Statement.java
@@ -0,0 +1,12 @@
+class Demo { 
+  public static void main(String[] args( {
+    int day = 2;
+    If (day == 1) {
+      System.Out.println("Monday");
+    } else if(day == 2) {
+      Syste.out.println("Tuesday");
+    } else if(day == 3) {
+      System.out.println("Wednesday");
+    }
+  }
+}
